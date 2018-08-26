24f8c6c1aa74e9ecf4c7741f28421594a4e38795
AndyVerona123 <andres.carrascal@udea.edu.co>
2018-08-26 14:05:53 -0500

    Avance del proyecyo

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20182/lab1/SplashScreen.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr05_20182/lab1/SplashScreen.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.R.*;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //layout.activity_splash_screen
         setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable() {
             @Override
